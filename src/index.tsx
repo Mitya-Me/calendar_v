351f4e81ad3d9c9a72dfb33fedab44091a08514c
mitya_me <dmitrymefodyev@gmail.com>
2022-01-17 05:38:45 +0300

    Create base store

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import {BrowserRouter} from "react-router-dom"
+import { store } from "./store";
 
 ReactDOM.render(
-    <Provider store={}>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
